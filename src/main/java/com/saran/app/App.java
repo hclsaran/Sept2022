195309b33da93f6871a2d3271db369b72cf69206
hclsaran <renusaran@hotmail.com>
2022-09-15 11:30:06 -0400

    newly created

---
+++ src/main/java/com/saran/app/App.java
@@ -0,0 +1,13 @@
+package com.saran.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to the  World of Java!" );
+    }
+}
